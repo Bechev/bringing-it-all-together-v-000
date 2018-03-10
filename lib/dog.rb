3153bddf233cfe26b4b4d535f334458015686b2c
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-10 21:47:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,6 @@ class Dog
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
-    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
     self
   end
